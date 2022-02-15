9a1c0637768020021692e36174d55276c65b6a71
sharonjency <99412360+sharonjency@users.noreply.github.com>
2022-02-15 16:23:00 +0530

    commit for violation test cases

--- src/test/java/ViolationSearch.java
+++ src/test/java/ViolationSearch.java
@@ -1,20 +1,40 @@
 package test.java;
-
 import main.java.pageEvents.LoginPageEvents;
 import main.java.pageEvents.ViolationSearchEvents;
 import org.testng.annotations.Test;
 
-public class ViolationSearch extends BaseTest {
+public class ViolationSearch extends BaseTest
+{
     ViolationSearchEvents violation = new ViolationSearchEvents();
+    LoginPageEvents login = new LoginPageEvents();
         @Test
-        public void navigateToViolationPage() throws InterruptedException {
+        public void navigateToViolationPage() throws InterruptedException
+        {
 
+            login.enterUsernamePassword();
+            login.clickOnSubmitButton();
             violation.navigateToViolationSearchTab();
             violation.verifyViolationPageTitle();
         }
         @Test
-        public void violationTypeSearch(){
+        public void violationTypeSearch() throws InterruptedException
+        {
+            login.enterUsernamePassword();
+            login.clickOnSubmitButton();
+            Thread.sleep(5000);
+            violation.navigateToViolationSearchTab();
             violation.selectViolationType();
             violation.clickOnViolaitionSearch();
+        }
+        @Test
+        public void setViolation() throws InterruptedException
+        {
+            login.enterUsernamePassword();
+            login.clickOnSubmitButton();
+            Thread.sleep(5000);
+            violation.navigateToViolationSearchTab();
+            violation.selectViolationType();
+            violation.clickOnViolaitionSearch();
+            violation.click360Propertyview();
         }
 }
